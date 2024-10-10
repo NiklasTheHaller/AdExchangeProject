6426e0eece087e6f81e3360243d2a7fe98c372fd
Niklas Haller <niklash2021@gmail.com>
2024-10-10 22:50:12 +0200

    Added eslint and some necessary files/directories

---
+++ Frontend/nuxt-ui-app/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<NuxtLink to="/">Ad Exchange</NuxtLink>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/registration">Registration</NuxtLink></li>
+				</ul>
+			</nav>
+		</header>
+
+		<div>
+			<slot />
+			<!-- Output Page Content -->
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.router-link-active {
+	color: #12b488; /* Active item */
+}
+</style>
