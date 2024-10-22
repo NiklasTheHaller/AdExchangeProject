f15a5e2a61a0acfdc83d845059a443fed6df457d
Clément <clemtoon78700.bri@gmail.com>
2024-10-22 19:22:09 +0200

    Feature : add a reset password page and enhanced the UI

--- Frontend/nuxt-ui-app/pages/reset-password.vue
+++ Frontend/nuxt-ui-app/pages/reset-password.vue
@@ -1,32 +1,22 @@
 <template>
     <div class='centerDiv'>
         <div class="max-w-md mx-auto">
-            <h1 class="text-2xl font-bold mb-6">Sign In to Ad Exchange</H1>
+            <h1 class="text-2xl font-bold mb-6">Forgot Password?</H1>
 
+            <p>
+                Enter the email address you used when you joined and we’ll send you instructions to reset your password.
+            </p>
             <UForm :schema="schema" :state="state" class="space-y-4">
 
-                <UFormGroup label="Email" name="email" size="xl">
+                <UFormGroup label="Email Address" name="email" class="mt-5">
+
                     <UInput type="email" placeholder="you@example.com" icon="i-heroicons-envelope" />
                     <!-- v-model="state.email"-->
                 </UFormGroup>
 
-                <UFormGroup label="Password" name="password" size="xl">
-                    <template #hint>
-                        <NuxtLink to="/reset-password" class="text-primary-500 underline">Forgot?</NuxtLink>
-                    </template>
-
-                    <!-- v-model="state.password"-->
-                    <UInput type="password" placeholder="Enter Password" icon="i-heroicons-lock-closed" />
-                </UFormGroup>
-
                 <UButton ui: type="submit" size="xl" class="login-button">
-                    Sign In
+                    Send Reset Instructions
                 </UButton>
-
-                <div class="inline-elements">
-                    <p>Don't have an account?</p>
-                    <ULink to="/registrationPage" class="text-primary-500 underline">Sign Up</ULink>
-                </div>
 
             </UForm>
         </div>
@@ -34,8 +24,7 @@
 </template>
 
 <script lang="ts">
-import type { _padding } from '#tailwind-config/theme';
-
+// TODO: Do a script that sent an email to reboot password to the right email adress
 
 </script>
 
